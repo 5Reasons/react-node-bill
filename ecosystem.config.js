2fc3025a92a5ecd80af252c084d77bea9ef2f020
Ruizheng <reason@RuizhengdeMacBook-Pro.local>
2022-05-25 15:45:31 +0800

    edit config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,8 +10,8 @@ module.exports = {
         user: 'root',
         host: '124.221.227.61',
         ref: 'origin/main',
-        repo: 'git@github.com:5Reasons/node-bill-server.git',
-        path: '/workspace/node-bill-server',
+        repo: 'git@github.com:5Reasons/react-node-bill.git',
+        path: '/workspace/react-node-bill',
         'post-deploy': 'git reset --hard && git checkout main && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
         env: {
           NODE_ENV: 'production'
